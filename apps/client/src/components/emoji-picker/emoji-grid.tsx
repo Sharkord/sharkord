2a3a692228d783bd757fef6f10e906351ae0c632
univeous <univeous@gmail.com>
2026-03-11 19:30:29 +0900

    chore: remove unused height parameter from EmojiGrid

--- apps/client/src/components/emoji-picker/emoji-grid.tsx
+++ apps/client/src/components/emoji-picker/emoji-grid.tsx
@@ -42,11 +42,10 @@ const EmojiButton = memo(({ emoji, onSelect }: TEmojiButtonProps) => {
 type TEmojiGridProps = {
   emojis: TEmojiItem[];
   onSelect: (emoji: TEmojiItem) => void;
-  height?: number;
 };
 
 const EmojiGrid = memo(
-  ({ emojis, onSelect, height = 280 }: TEmojiGridProps) => {
+  ({ emojis, onSelect }: TEmojiGridProps) => {
     const rowCount = useMemo(
       () => Math.ceil(emojis.length / GRID_COLS),
       [emojis.length]
