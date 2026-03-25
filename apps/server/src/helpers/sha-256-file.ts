1b422055f0caed69f0a1827739df29858c9cd6e5
Diogo Martino <diogo.martino10@gmail.com>
2026-03-25 19:41:04 +0000

    fix: types

--- apps/server/src/helpers/sha-256-file.ts
+++ apps/server/src/helpers/sha-256-file.ts
@@ -2,7 +2,7 @@ const sha256File = async (filePath: string) => {
   const hasher = new Bun.CryptoHasher('sha256');
 
   const file = Bun.file(filePath);
-  const stream = file.stream();
+  const stream = file.stream() as unknown as AsyncIterable<Uint8Array>;
 
   for await (const chunk of stream) {
     hasher.update(chunk);
