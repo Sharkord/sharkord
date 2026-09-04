4b9281caadf401fe16a7b7238a711240022d40ab
Diogo Martino <diogo.martino10@gmail.com>
2026-09-04 18:36:07 +0100

    fix: missing permission gate regression

--- apps/server/src/routers/messages/toggle-message-pin.ts
+++ apps/server/src/routers/messages/toggle-message-pin.ts
@@ -1,3 +1,4 @@
+import { Permission } from '@sharkord/shared';
 import { z } from 'zod';
 import { loadMessageForWrite } from '../../helpers/load-message-for-write';
 import { setMessagePinned } from '../../helpers/message-pin';
@@ -6,6 +7,8 @@ import { protectedProcedure } from '../../utils/trpc';
 const toggleMessagePinRoute = protectedProcedure
   .input(z.object({ messageId: z.number() }))
   .mutation(async ({ ctx, input }) => {
+    await ctx.needsPermission(Permission.PIN_MESSAGES);
+
     const message = await loadMessageForWrite(ctx, input.messageId);
 
     await setMessagePinned(message, !message.pinned, ctx.user.id);
